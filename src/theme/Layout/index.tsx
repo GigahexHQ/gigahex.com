3cc1cc03a3b7c17809c9e46f5a306b85f087a643
Shadab <shadab19it@gmail.com>
2021-05-25 10:36:37 +0530

    its create problem in stating the app so I comment it

--- src/theme/Layout/index.tsx
+++ src/theme/Layout/index.tsx
@@ -1,1 +1,11 @@
-../PageLayout/index.tsx
+// import PageLayout from "../PageLayout/index"
+
+// exports <PageLayout />
+
+import React from "react"
+
+const index = () => {
+  return <div />
+}
+
+export default index
