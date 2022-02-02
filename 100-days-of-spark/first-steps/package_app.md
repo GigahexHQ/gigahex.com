2004b614606d3711967dc30e60d778b821780902
Shad Amez <shad.amezng@gmail.com>
2022-02-02 20:12:30 +0530

    added day-3 guide

--- 100-days-of-spark/first-steps/package_app.md
+++
@@ -1,5 +0,0 @@
----
-id: package-spark-app
-title: Day 03 - Package your application
-sidebar_label: Day 03 - Package your application
----
--- sidebars_spark.js
+++ sidebars_spark.js
@@ -17,6 +17,10 @@ if (process.env.NODE_ENV === "development") {
 module.exports = {
   spark_mastery: {
     Introduction: ["spark-intro", "get-started"],
-    "First Steps": ["first-steps/setup-spark", "first-steps/write-to-file"],
+    "First Steps": [
+      "first-steps/setup-spark",
+      "first-steps/write-to-file",
+      "first-steps/package-spark-app",
+    ],
   },
 }
